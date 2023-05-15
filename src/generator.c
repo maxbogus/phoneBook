3055245a7029fc9a429110b5f80f730f23376585
Max Boguslavsky <521205+maxbogus@users.noreply.github.com>
2023-05-15 09:32:24 +0300

    Update generator.c
    - removed excessive imports;
    - added surnames;
    - made more realistic phone generation.

--- src/generator.c
+++ src/generator.c
@@ -1,71 +1,37 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <assert.h>
 #include "generator.h"
 
 char names[24][10] = {
-    "Maria",
-    "Nushi",
-    "Mohammed",
-    "Jose",
-    "Wei",
-    "Ahmed",
-    "Yan",
-    "Ali",
-    "John",
-    "David",
-    "Abdul",
-    "Ana",
-    "Ying",
-    "Michael",
-    "Juan",
-    "Mary",
-    "Jean",
-    "Robert",
-    "Daniel",
-    "Luis",
-    "Carlos",
-    "James",
-    "Antonio",
-    "Joseph",
-};
+    "Maria", "Nushi", "Mohammed", "Jose", "Wei", "Ahmed", "Yan", "Ali", "John", "David", "Abdul", "Ana", "Ying",
+    "Michael", "Juan", "Mary", "Jean", "Robert", "Daniel", "Luis", "Carlos", "James", "Antonio", "Joseph"};
 
-char surnames[17][11] = {"Wang",
-                         "Smith", "Devi",
-                         "Ivanov",
-                         "Kim", "Ali",
-                         "García",
-                         "Müller", "Mohamed",
-                         "Tesfaye",
-                         "Nguyen", "Ilunga",
-                         "González",
-                         "Deng", "Rodríguez",
-                         "Moyo",
-                         "Hansen"};
+char surnames[37][11] = {
+    "Wang", "Smith", "Devi", "Ivanov", "Kim", "Ali", "García", "Müller", "Mohamed", "Tesfaye", "Nguyen", "Ilunga",
+    "González", "Deng", "Rodríguez", "Moyo", "Hansen", "Hoxha", "Smirnov", "Kuznetsov", "Popov", "Sokolov",
+    "Lebedev", "Kozlov", "Novikov", "Morozov", "Petrov", "Volkov", "Solovyov", "Vasilyev", "Zaytsev", "Pavlov",
+    "Semyonov", "Golubev", "Vinogradov", "Bogdanov", "Vorobyov"};
 
 void generatePhone(char *str, int limit)
 {
     int index;
+    int charNumber = 8;
     for (index = 0; index <= limit; index++)
     {
-        int charNumber = 8;
-        if (index == 0)
+        switch (index)
         {
-        }
-        else if (index == 1)
-        {
-            charNumber = 9;
-        }
-        else if (index == 2)
-        {
+        case 0:
+            break;
+        case 1:
+            charNumber = 4;
+            break;
+        case 2:
             charNumber = index % 3;
-        }
-        else
-        {
+            break;
+        default:
             charNumber = rand() % 9;
+            break;
         }
-
         str[index] = (charNumber) + '0';
     }
 }
@@ -77,12 +43,12 @@ int main()
     {
         printf("Started file generation:\n");
         int index;
-        fprintf(file, "ID,Phonenumber,Name,Surname\n");
+        fprintf(file, "Phonenumber,Name,Surname\n");
         for (index = 0; index < FILE_SIZE; index++)
         {
-            char phone[11];
-            generatePhone(phone, 11);
-            fprintf(file, "%d,%s,%s,%s\n", index, phone, names[index % 23], surnames[index % 16]);
+            char phone[10];
+            generatePhone(phone, 10);
+            fprintf(file, "%s,%s,%s\n", phone, names[index % 23], surnames[index % 36]);
         }
         printf("Generation complete.\n");
         fclose(file);
