c998f8656d349946764dc79949089e77e47e7f42
Max Boguslavsky <521205+maxbogus@users.noreply.github.com>
2023-05-15 17:31:41 +0300

    Update main.h
    - sorted consts.

--- src/main.h
+++ src/main.h
@@ -1,6 +1,9 @@
 #define BUFFERSIZE 15
-#define PHONEBOOK_SIZE 1024
+#define CONFIRM_MODIFICATION "Yes (1) | No (Other)"
 #define PHONEBOOK_BASE_NAME "phonebook.csv"
+#define PHONEBOOK_SIZE 1024
+#define SELECT_ENTRY_TO_MODIFY "What entry do you want to modify?"
+#define SELECT_FIELD_TO_MODIFY "What field do you want to modify? 1 - Phone; 2 - Name; 3 - Surname; Other - exit"
 
 struct PhoneEntry
 {
