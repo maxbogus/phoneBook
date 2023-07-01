6fae509a5f0cf953de73447c1fd3e7effd6814db
Maxim Boguslavsky <521205+maxbogus@users.noreply.github.com>
2023-07-01 16:07:08 +0300

    Update main.c
    - rewrote app to use hashMap;
    - lowered amount of collisions while working with it.

--- src/main.c
+++ src/main.c
@@ -1,63 +1,131 @@
-#include <assert.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <math.h>
 
 #include "main.h"
 
-void getInput(char *buffer, const char *text)
+struct PhoneEntry *phoneBook;
+struct PhoneEntry *tempPhoneBook;
+int capacity = 10;
+int size = 0;
+int charLimit = 15;
+char emptyString[15] = {'\0'};
+
+int hashFunction(int key)
 {
-    fflush(stdin);
-    printf("%s\n", text);
-    while (true)
+    return (key % capacity);
+}
+
+int hashFunctionLong(long key)
+{
+    return (key % capacity);
+}
+
+int checkPrime(int n)
+{
+    if (n <= 1)
+        return 0;
+    if (n <= 3)
+        return 1;
+
+    if (n % 2 == 0 || n % 3 == 0)
+        return 0;
+
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return 0;
+
+    return 1;
+}
+
+int getPrime(int n)
+{
+    if (n % 2 == 0)
     {
-        assert(fgets(buffer, BUFFERSIZE, stdin) != NULL);
-        if (buffer[strlen(buffer) - 1] == '\n')
+        n++;
+    }
+    while (!checkPrime(n))
+    {
+        n += 2;
+    }
+    return n;
+}
+
+void insert(int key, char phoneNumber[charLimit], char firstName[charLimit], char secondName[charLimit])
+{
+    int index = hashFunction(key);
+    if (
+        strlen(phoneBook[index].phoneNumber) == 0 && strlen(phoneBook[index].firstName) == 0 && strlen(phoneBook[index].secondName) == 0)
+    {
+        phoneBook[index].key = key;
+        strcpy(phoneBook[index].phoneNumber, phoneNumber);
+        strcpy(phoneBook[index].firstName, firstName);
+        strcpy(phoneBook[index].secondName, secondName);
+        size++;
+        printf("\n Key (%d) has been inserted \n", key);
+    }
+    else if (phoneBook[index].key == key)
+    {
+        strcpy(phoneBook[index].phoneNumber, phoneNumber);
+        strcpy(phoneBook[index].firstName, firstName);
+        strcpy(phoneBook[index].secondName, secondName);
+    }
+    else
+    {
+        printf("\n Collision occured  \n");
+    }
+}
+
+void remove_element(int key)
+{
+    int index = hashFunction(key);
+    if (
+        strlen(phoneBook[index].phoneNumber) == 0 && strlen(phoneBook[index].firstName) == 0 && strlen(phoneBook[index].secondName) == 0)
+    {
+        printf("\n This key does not exist \n");
+    }
+    else
+    {
+        phoneBook[index].key = 0;
+        strcpy(phoneBook[index].phoneNumber, emptyString);
+        strcpy(phoneBook[index].firstName, emptyString);
+        strcpy(phoneBook[index].secondName, emptyString);
+        size--;
+        printf("\n Key (%d) has been removed \n", key);
+    }
+}
+
+void display()
+{
+    int index;
+    for (index = 0; index < capacity; index++)
+    {
+        if (strlen(phoneBook[index].phoneNumber) != 0)
         {
-            buffer[strlen(buffer) - 1] = '\0';
-            break;
+            printf("\n array[%d]: %s, %s, %s \t", index, phoneBook[index].phoneNumber, phoneBook[index].firstName, phoneBook[index].secondName);
         }
     }
 }
 
-void addEntry(struct PhoneEntry *entries, int lastFreeIndex)
+void displayBackward()
 {
-    struct PhoneEntry entry = {};
-    getInput(entry.phoneNumber, "Enter phone number:\n");
-    getInput(entry.firstName, "Enter name:\n");
-    getInput(entry.secondName, "Enter surname:\n");
-    printf("%s", "Output:");
-    printf("%s %s %s \n", entry.phoneNumber, entry.firstName, entry.secondName);
-    entries[lastFreeIndex] = entry;
-    printf("Press Any Key to Continue\n");
-    getchar();
-    fflush(stdin);
+    int index;
+    for (index = capacity - 1; index >= 0; index--)
+    {
+        if (strlen(phoneBook[index].phoneNumber) != 0)
+        {
+            printf("\n array[%d]: %s, %s, %s \t", index, phoneBook[index].phoneNumber, phoneBook[index].firstName, phoneBook[index].secondName);
+        }
+    }
 }
 
-int getSelection(const char *question)
+int size_of_hashtable()
 {
-    fflush(stdin);
-    int selectedField = 0;
-    printf("%s\n", question);
-    selectedField = getchar();
-    return selectedField - '0';
+    return size;
 }
 
-enum AppState
-{
-    Add,
-    Edit,
-    Search,
-    Export,
-    Import,
-    View,
-    Sort,
-    Exit
-};
-
-void saveToFile(const struct PhoneEntry *entries, int lastFreeIndex)
+void saveToFile()
 {
     FILE *phonebook;
     phonebook = fopen(PHONEBOOK_BASE_NAME, "w");
@@ -67,9 +135,9 @@ void saveToFile(const struct PhoneEntry *entries, int lastFreeIndex)
     {
         fprintf(phonebook, "Phonenumber, Name, Surname\n");
         int index;
-        for (index = 0; index <= lastFreeIndex; index++)
+        for (index = 0; index <= size; index++)
         {
-            struct PhoneEntry entry = entries[index];
+            struct PhoneEntry entry = phoneBook[index];
             fprintf(phonebook, "%s, %s, %s", entry.phoneNumber,
                     entry.firstName, entry.secondName);
         }
@@ -77,7 +145,52 @@ void saveToFile(const struct PhoneEntry *entries, int lastFreeIndex)
     }
 }
 
-int loadData(struct PhoneEntry *entries)
+long convertPhone(char *phone)
+{
+    long result = 0;
+    int index;
+
+    for (index = 0; index < ((int)strlen(phone)); index++)
+    {
+        int number = phone[index] - '0';
+        long power = (long)pow(10, strlen(phone) - (index + 1));
+        long mult = number * power;
+        result += mult;
+    }
+
+    return result;
+}
+
+void init_array()
+{
+    capacity = size;
+    capacity = getPrime(capacity * 20);
+    phoneBook = (struct PhoneEntry *)malloc(capacity * sizeof(struct PhoneEntry));
+    for (int index = 0; index < capacity; index++)
+    {
+        phoneBook[index].key = 0;
+        strcpy(phoneBook[index].phoneNumber, emptyString);
+        strcpy(phoneBook[index].firstName, emptyString);
+        strcpy(phoneBook[index].secondName, emptyString);
+    }
+}
+
+void fill_array()
+{
+    phoneBook = (struct PhoneEntry *)malloc(capacity * sizeof(struct PhoneEntry));
+    for (int index = 0; index < size; index++)
+    {
+        long convertedPhone = convertPhone(tempPhoneBook[index].phoneNumber);
+        int phoneBookIndex = hashFunctionLong(convertedPhone);
+
+        phoneBook[phoneBookIndex].key = phoneBookIndex;
+        strcpy(phoneBook[phoneBookIndex].phoneNumber, tempPhoneBook[index].phoneNumber);
+        strcpy(phoneBook[phoneBookIndex].firstName, tempPhoneBook[index].firstName);
+        strcpy(phoneBook[phoneBookIndex].secondName, tempPhoneBook[index].secondName);
+    }
+}
+
+int loadData()
 {
     FILE *phonebook = fopen(PHONEBOOK_BASE_NAME, "r");
     int index = 0;
@@ -85,13 +198,29 @@ int loadData(struct PhoneEntry *entries)
         printf("Can't open file\n");
     else
     {
-        char buffer[PHONEBOOK_SIZE];
+        printf("Import started.\n");
         int row = 0;
+        tempPhoneBook = (struct PhoneEntry *)calloc(capacity, sizeof(struct PhoneEntry));
+        char buffer[1024];
         while (fgets(buffer,
-                     PHONEBOOK_SIZE, phonebook))
+                     1024, phonebook))
         {
+            if (row >= capacity)
+            {
+                capacity += 1;
+                struct PhoneEntry *temp;
+                temp = (struct PhoneEntry *)realloc(tempPhoneBook, capacity * sizeof(struct PhoneEntry));
+                if (temp != NULL)
+                {
+                    tempPhoneBook = temp;
+                }
+                else
+                {
+                    printf("problem");
+                    return -1;
+                }
+            }
             char *value = strtok(buffer, ", ");
-            index = row - 1;
             if (row != 0)
             {
                 int column;
@@ -114,8 +243,10 @@ int loadData(struct PhoneEntry *entries)
                     if (column == 2)
                     {
                         strcpy(entry.secondName, value);
-                        entries[index] = entry;
-                        printf("phone: %s firstName: %s surname: %s index = %d\n", entries[index].phoneNumber, entries[index].firstName, entries[index].secondName, index);
+                        index = row - 1;
+                        entry.key = index;
+                        tempPhoneBook[index] = entry;
+                        // printf("phone: %s firstName: %s surname: %s index = %d\n", tempPhoneBook[index].phoneNumber, tempPhoneBook[index].firstName, tempPhoneBook[index].secondName, index);
                     }
                     value = strtok(NULL, ", ");
                     column++;
@@ -123,176 +254,116 @@ int loadData(struct PhoneEntry *entries)
             }
             row++;
         }
-
+        printf("Import completed.\n");
         fclose(phonebook);
     }
-    return index;
-}
 
-void showEntries(struct PhoneEntry *entries, int lastFreeIndex)
-{
-    if (lastFreeIndex > 0)
-    {
-        int index = 0;
-        while (index <= lastFreeIndex)
-        {
-            printf("phone: %s firstName: %s surname: %s \n", entries[index].phoneNumber, entries[index].firstName, entries[index].secondName);
-            index++;
-        }
-    }
-    else
-    {
-        printf("No entries. \n");
-    }
-}
-
-void editEntry(struct PhoneEntry *entries, int lastFreeIndex)
-{
-    struct PhoneEntry entry;
-    bool exit = false;
-    while (!exit)
-    {
-        printf("Entries to modify: 0 - %d \n", lastFreeIndex);
-        int index = getSelection(SELECT_ENTRY_TO_MODIFY);
-        if (0 <= index || index <= lastFreeIndex)
-        {
-            printf("Current entry:\n");
-            entry = entries[index];
-            printf("Phone: %s \tName: %s \t Surname: %s.", entry.phoneNumber, entry.firstName, entry.secondName);
-
-            int selection = getSelection(SELECT_FIELD_TO_MODIFY);
-            char phoneNumber[15];
-            char firstName[15];
-            char secondName[15];
-            strcpy(phoneNumber, entry.phoneNumber);
-            strcpy(firstName, entry.firstName);
-            strcpy(secondName, entry.secondName);
-            switch (selection)
-            {
-            case 1:
-                printf("Current value: %s\n", entry.phoneNumber);
-                getInput(phoneNumber, "Enter new value:");
-                break;
-            case 2:
-                printf("Current value: %s\n", entry.firstName);
-                getInput(firstName, "Enter new value:");
-                break;
-            case 3:
-                printf("Current value: %s\n", entry.secondName);
-                getInput(secondName, "Enter new value:");
-                break;
-            };
-
-            int apply = 0;
-            apply = getSelection(CONFIRM_MODIFICATION);
-            if (apply == 1)
-            {
-                strcpy(entry.phoneNumber, phoneNumber);
-                strcpy(entry.firstName, firstName);
-                strcpy(entry.secondName, secondName);
-                printf("\nPhone: %s \tName: %s \t Surname: %s.", entry.phoneNumber, entry.firstName, entry.secondName);
-                entries[index] = entry;
-            }
-            exit = true;
-        }
-    }
-}
-
-enum AppState selectState(int input)
-{
-    enum AppState appState;
-    switch (input)
-    {
-    case 49:
-        appState = Add;
-        break;
-    case 50:
-        appState = Edit;
-        break;
-    case 51:
-        appState = Sort;
-        break;
-    case 52:
-        appState = View;
-        break;
-    case 53:
-        appState = Export;
-        break;
-    case 54:
-        appState = Search;
-        break;
-    case 55:
-        appState = Import;
-        break;
-    default:
-        appState = Exit;
-    }
-    return appState;
-}
-
-void printMenu()
-{
-    // system("clear");
-    printf("Welcome to the Phone book app!\n");
-    printf("Select mode:\n");
-    printf("1 - Add\n");
-    printf("2 - Edit\n");
-    printf("3 - Sort\n");
-    printf("4 - View\n");
-    printf("5 - Export\n");
-    printf("6 - Search\n");
-    printf("7 - Import\n");
-    printf("0 - Exit\n");
+    return index + 1;
 }
 
 int main()
 {
-    // TODO: Sort struct
-    // TODO: Search struct
-    // TODO: allocate file dynamically while loading file
+    int choice, key, n;
+    char phoneNumber[charLimit], firstName[charLimit], secondName[charLimit];
 
-    // generate test data
-    int exitCode = 0;
-    enum AppState appState = Import;
-    struct PhoneEntry entries;
-    int lastFreeIndex = 0;
+    int c = 0;
+    size = loadData();
 
-    while (appState != Exit)
+    init_array();
+    fill_array();
+
+    do
     {
-        switch (appState)
+        printf("Welcome to the Phone book app!"
+               "\n1 - Add|Edit item"
+               "\n2 - Delete item"
+               "\n3 - Show number of items in hashTable"
+               "\n4 - View (Desc)"
+               "\n5 - View (Asc)"
+               "\n6 - Search by phone"
+               "\n7 - Export"
+               "\n8 - Import"
+               "\n\n Please enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
         {
-        case Add:
-            addEntry(&entries, lastFreeIndex);
-            lastFreeIndex++;
-            break;
-        case Edit:
-            editEntry(&entries, lastFreeIndex);
-            break;
-        case Sort:
-            printf("not implemented \n");
-            break;
-        case View:
-            showEntries(&entries, lastFreeIndex);
-            break;
-        case Export:
-            saveToFile(&entries, lastFreeIndex);
-            break;
-        case Search:
-            printf("not implemented \n");
-            break;
-        case Import:
-            lastFreeIndex = loadData(&entries);
-            break;
-        case Exit:
-            return 0;
+        case 1:
+        {
+            fflush(stdin);
+            printf("Enter phone number -:\t");
+            scanf("%11[^\n]%*c", phoneNumber);
+            fflush(stdin);
+            printf("Enter first name -:\t");
+            scanf("%14[^\n]%*c", firstName);
+            fflush(stdin);
+            printf("Enter second name -:\t");
+            scanf("%14[^\n]%*c", secondName);
+            fflush(stdin);
+
+            long convertedPhone = convertPhone(phoneNumber);
+            insert(hashFunctionLong(convertedPhone), phoneNumber, firstName, secondName);
+
             break;
         }
-        printMenu();
-        int input;
-        input = getchar();
-        appState = selectState(input);
-        fflush(stdin);
-    }
+        case 2:
+        {
+            printf("Enter the key to delete-:");
+            scanf("%d", &key);
+            remove_element(key);
 
-    return exitCode;
+            break;
+        }
+        case 3:
+        {
+            n = size_of_hashtable();
+            printf("Size of Hash Table is-:%d\n", n);
+
+            break;
+        }
+        case 4:
+        {
+            displayBackward();
+
+            break;
+        }
+        case 5:
+        {
+            display();
+        }
+        case 6:
+        {
+            fflush(stdin);
+            printf("Enter phone number -:\t");
+            scanf("%11[^\n]%*c", phoneNumber);
+            long convertedPhone = convertPhone(phoneNumber);
+            int index = hashFunctionLong(convertedPhone);
+            struct PhoneEntry entry = phoneBook[index];
+            printf("%d: %s %s %s", index, entry.phoneNumber, entry.firstName, entry.secondName);
+            break;
+        }
+        case 7:
+        {
+            saveToFile();
+
+            break;
+        }
+        case 8:
+        {
+            size = loadData();
+            init_array();
+            fill_array();
+
+            break;
+        }
+        default:
+        {
+            printf("Invalid Input\n");
+        }
+        }
+
+        printf("\nDo you want to continue (press 1 for yes): ");
+        scanf("%d", &c);
+
+    } while (c == 1);
 }
