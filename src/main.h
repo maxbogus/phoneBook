d2b3bf8658db1285a3cc5ee7438f2ee1c8d62034
Maxim Boguslavsky <521205+maxbogus@users.noreply.github.com>
2023-07-01 16:07:42 +0300

    Update main.h
    - removed messages.

--- src/main.h
+++ src/main.h
@@ -1,12 +1,9 @@
 #define BUFFERSIZE 15
-#define CONFIRM_MODIFICATION "Yes (1) | No (Other)"
 #define PHONEBOOK_BASE_NAME "phonebook.csv"
-#define PHONEBOOK_SIZE 1024
-#define SELECT_ENTRY_TO_MODIFY "What entry do you want to modify?"
-#define SELECT_FIELD_TO_MODIFY "What field do you want to modify? 1 - Phone; 2 - Name; 3 - Surname; Other - exit"
 
 struct PhoneEntry
 {
+    int key;
     char phoneNumber[BUFFERSIZE];
     char firstName[BUFFERSIZE];
     char secondName[BUFFERSIZE];
